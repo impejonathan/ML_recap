069e495bbf5cf47e25846e72d0e78170b54878d0
Ahmaddarwiche <darwich.ahmad.92@gmail.com>
2023-08-08 16:28:49 +0200

    blabla

--- allocine_films/allocine_films/spiders/scraping_vues.py
+++ allocine_films/allocine_films/spiders/scraping_vues.py
@@ -1,12 +1,12 @@
 import scrapy
 
 class MoviesSpider(scrapy.Spider):
-    name = 'moviesspider'
+    name = 'vuesspider'
     allowed_domains = ['allocine.fr']
 
     def start_requests(self):
         base_url = 'https://www.allocine.fr/films/?page={}'
-        # Loop to iterate through pages from 1 to 7693
+        # Loop to iterate through pages from 1 to 2 (for testing)
         for page_number in range(1, 2):
             url = base_url.format(page_number)
             yield scrapy.Request(url, self.parse)
@@ -16,22 +16,23 @@ class MoviesSpider(scrapy.Spider):
             title = movie.css('::text').get().strip()
 
             # Extracting video link
-            video_link = movie.css('a.thumbnail-link::attr(href)').get()
+            video_link = movie.css('a.thumbnail-container.thumbnail-link::attr(href)').get()
+            # video_link = movie.xpath('.//a[contains(@class, "thumbnail-container thumbnail-link")]/@href').get()
             if video_link:
                 yield scrapy.Request(video_link, callback=self.parse_movie_page, meta={'title': title})
-            print(video_link, title)
+            print('lalalalalalalallallalalallalallalalall LALALALLLALALLALALALALALAL',  video_link, title)
 
     def parse_movie_page(self, response):
         title = response.meta['title']
 
         # Extracting views
-        views_text = response.css('.media-info-item.icon-eye::text').get()
+        views_text = response.css('div.media-info-item.icon.icon-eye::text').get()
         if views_text:
             views = views_text.split()[0]  # Extract the number from "31 251 vues"
-
         else:
             views = 'N/A'
-        print(views)
+        print('mimmimimmimimimimimmimimimmimimimmimimi', views)
+        
         yield {
             'Titre': title,
             'Vues': views
