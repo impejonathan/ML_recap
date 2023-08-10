e69a5d74e3bc3abd0464495fc3c9fdd48bbc5db0
Ahmaddarwiche <darwich.ahmad.92@gmail.com>
2023-08-10 12:21:42 +0200

    nettoyage

--- run.py
+++
@@ -1,9 +0,0 @@
-from app.main import app as fastapi_app
-
-
-from app.main import app as fastapi_app
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000)
